9a1e4e7fe8c9338ff55d2d3c8e199ba7d53eacc1
prof <prof@Pracownia12>
2018-09-18 09:43:03 +0200

    Merge origin/master
    Conflicts:
    	cw1/cw1.html
    	index.html

---
+++ cw6/cw6.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        $workers=[
+            ["Jan","Nowak",4500], //0
+            ["Anna","Wanna",3900], //1
+            ["Monika","Klika",6900],//2
+            ["Tomasz","Bomasz",4400]//3
+        ];
+        //TODO   $workers[1][0] ---> "Anna";
+        function WorkersToTable(array $dane){
+            //do tabelki
+        }
+        echo WorkersToTable($workers);
+        ?>
+    </body>
+</html>
