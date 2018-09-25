e9211c2190fa671a822e5323e6d9d650cfee4c9c
prof <prof@Pracownia12>
2018-09-25 12:14:56 +0200

    Merge origin/master
    Conflicts:
    	cw1/cw1.html
    	index.html

--- cw8/zapisz.php
+++ cw8/zapisz.php
@@ -5,10 +5,30 @@
         <title></title>
     </head>    
     <body>
-        <pre>
+        
         <?php
-        print_r($_POST);
+        if(!isset($_POST['imie'])){
+            exit();
+        }
+        $imie = $_POST['imie'];
+        $nazwisko = $_POST['nazwisko'];
+        $klasa = $_POST['klasa'];
+        $plec = $_POST['plec'];
+        $zaint = isset($_POST['zaint'])? $_POST['zaint']:[];
+        if(trim($imie)=='' || trim($nazwisko)==''){
+            echo "<p>Brak poprawnych danych</p>\n";
+            exit();
+        }
+        $html = "<h1>Informacje o rejestracji</h1>\n";
+        $html .= "<p>Nazwisko: {$nazwisko}  "
+                . "Imię: {$imie} Płeć: {$plec} "
+                . "Klasa: {$klasa}</p>\n";
+        if(count($zaint)>0){
+            $html .= "<p>Twoje zainteresowania: "
+                    .implode(', ', $zaint) . "</p>\n";
+        } 
+        echo $html;
         ?>
-    </pre>
+    
     </body>
 </html>
