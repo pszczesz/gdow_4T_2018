d7f906e2e2e42c3f8fdb2b5ef11f2953b49ecfa8
prof <prof@Pracownia12>
2018-09-25 09:34:43 +0200

    Merge origin/master
    Conflicts:
    	cw1/cw1.html
    	index.html

---
+++ cw8/zapisz.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>    
+    <body>
+        <pre>
+        <?php
+        print_r($_POST);
+        ?>
+    </pre>
+    </body>
+</html>
